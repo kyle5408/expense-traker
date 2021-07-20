3f4d87680b3f3d76cb760ce65fcab41fa94207bc
kyle5408 <kyle5408@yahoo.com.tw>
2021-07-20 19:15:50 +0800

    feat: totalAmount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,13 +6,16 @@ const Category = require('../../config/category.json')
 const categories = Category.results
 
 
-
 router.get('/', (req, res) => {
+  let totalAmount = 0
   Record.find()
     .lean()
     .sort({ _id: 'asc' })
     .then(records => {
-      res.render('index', { records, categories })
+      records.forEach(record => {
+        totalAmount += record.amount
+      })
+      res.render('index', { records, categories, totalAmount })
     })
     .catch(error => console.error(error))
 })
