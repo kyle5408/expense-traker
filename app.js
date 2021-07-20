c1e35f9a8ee469016eb75e46605f2e0b3563d9e9
kyle5408 <kyle5408@yahoo.com.tw>
2021-07-20 14:22:27 +0800

    feat: main.hbs & rotues & Schema

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const app = express()
+const routes = require('./routes')
+require('./config/mongoose')
+
+const PORT = process.env.PORT || 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`The server is running in http://localhost:${PORT}`)
+})
