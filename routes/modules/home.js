c14f76c473503175a8a7820aa3ccd03a978b1c86
kyle5408 <kyle5408@yahoo.com.tw>
2021-07-20 15:03:55 +0800

    fix: home.js router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,11 +4,11 @@ const router = express.Router()
 const Record = require('../../models/record')
 
 router.get('/', (req, res) => {
-  Record.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ _id: 'asc' }) //正序asc 反序desc
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
+  Record.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(records => res.render('index', { records }))
+    .catch(error => console.error(error))
 })
 
 module.exports = router
