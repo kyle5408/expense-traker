45852c99459642e901d9e2bde7eb3bf62425ace2
kyle5408 <kyle5408@yahoo.com.tw>
2021-07-22 15:17:09 +0800

    fix: edit.hbs & records.js→add express validator

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -26,21 +26,20 @@ router.get('/:id/edit', (req, res) => {
 
 router.put('/:id', [checkParams('name', '編輯失敗：未輸入名稱').isLength({ min: 1 }),
 checkParams('date', '編輯失敗：未選擇日期').isLength({ min: 1 }),
-checkParams('category', '編輯失敗：未選擇類別').custom(value => value !== "Select"),
+checkParams('category', '編輯失敗：類別無法為空').custom(value => value !== "Select"),
 checkParams('amount', '編輯失敗：金額錯誤').custom(value => value > 0)
 ], (req, res) => {
   const id = req.params.id
   const errorsResult = validationResult(req)
   if (!errorsResult.isEmpty()) {
-    const { name, date, category, amount } = req.body
-    console.log(req.body)
+    const { name, date, amount } = req.body
     const errorMsg = errorsResult.array()
     return Record.findById(id)
       .lean()
       .then(record => {
         //使用moment處理日期
         record.date = moment(record.date).format('YYYY-MM-DD')
-        res.render('edit', { name, date, category, amount, record, errorMsg: errorMsg })
+        res.render('edit', { name, date, amount, record, errorMsg: errorMsg })
       })
 
 
