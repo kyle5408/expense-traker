2e34b4bb80738b3d6a8725665dc15c069336804b
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-09 16:48:13 +0800

    fix: passport function (app.js & passport.js)

--- config/passport.js
+++ config/passport.js
@@ -5,11 +5,11 @@ const User = require('../models/user')
 module.exports = app => {
   // 初始化 Passport 模組
   app.use(passport.initialize())
-  app.use(passport.deserializeUser())
+  app.use(passport.session())
   // 設定本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email' },
     (email, password, done) => {
-      User.findOne({ user })
+      User.findOne({ email })
         .then(user => {
           if (!user) {
             return done(null, false, { message: 'That email is not registered!' })
@@ -19,7 +19,7 @@ module.exports = app => {
           }
           return done(null, user)
         })
-        .catch(err => done(err))
+        .catch(err => done(err, false))
     })
   )
   // 設定序列化與反序列化
@@ -30,9 +30,6 @@ module.exports = app => {
     User.findById(id)
       .lean()
       .then(user => done(null, user))
-      .catch(err => done(err))
+      .catch(err => done(err, null))
   })
-
-
-
 }
