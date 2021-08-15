a36e26e6d553f415d91711e1ac70901cf34c415f
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-15 16:05:14 +0800

    feat: add google login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
@@ -30,6 +31,33 @@ module.exports = app => {
       .catch(err => done(err, false))
   })
   )
+
+  //設定Google登入策略
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.callbackURL
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }
+  ))
+
+
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
     done(null, user._id)
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
 const filter = require('./modules/filter')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
-
-
 
 router.use('/records', authenticator, records)
 router.use('/filter', authenticator, filter)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
